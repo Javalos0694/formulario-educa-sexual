bfa10ea8ae5b7ec212e58cdf3f32ae79411db429
Javalos0694 <joseph.avalos0694@gmail.com>
2020-09-23 20:39:01 -0500

    http module

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express();
+const http = require('http');
 
+const server = http.createServer(app);
 
 app.use(express.static('FrontEnd'));
  
 
-app.listen(process.env.PORT || 3800, ()=>{
+server.listen(process.env.PORT || 3800, ()=>{
     console.log('server corriendo')
 })
